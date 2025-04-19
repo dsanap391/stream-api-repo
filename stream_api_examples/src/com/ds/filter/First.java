89496eeed8db65d25994af95b4b2da213698acac
dsanap <dsanap391@gmail.com>
2025-04-19 15:59:26 +0530

    Find the name starting with a given character

--- stream_api_examples/src/com/ds/filter/First.java
+++ stream_api_examples/src/com/ds/filter/First.java
@@ -1,9 +1,16 @@
 package com.ds.filter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Find the name starting with character 'A'
+
 public class First {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<String> names = List.of("Alice", "Bob", "Ankit", "Arun", "Steve","Chinamay","Devansh");
+		List<String> listNames = names.stream().filter(n -> n.startsWith("A")).collect(Collectors.toList());
+		
+		System.out.println(listNames);
 	}
 }
