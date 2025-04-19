ef19c0c49abe39e158ac4152a9105b9f1e35d8e6
dsanap <dsanap391@gmail.com>
2025-04-19 17:45:53 +0530

    Remove Empty Strings from list of strings

---
+++ stream_api_examples/src/com/ds/filter/Third.java
@@ -0,0 +1,15 @@
+package com.ds.filter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Remove empty strings from list of string
+
+public class Third {
+
+	public static void main(String[] args) {
+		List<String> names = List.of("Alice", "Arun", "Steve", "Chinamay", "Devansh", "","Haridas", "", "Jullie");
+		List<String> list = names.stream().filter(n -> !n.isEmpty()).collect(Collectors.toList());
+		System.out.println(list);
+	}
+}
