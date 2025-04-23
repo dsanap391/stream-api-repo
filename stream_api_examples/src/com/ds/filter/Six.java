01c7da1ee715e4bd4eef793d76818473037eee87
dsanap <dsanap391@gmail.com>
2025-04-23 07:25:30 +0530

    filtering custom objects based on condition

--- stream_api_examples/src/com/ds/filter/Six.java
+++ stream_api_examples/src/com/ds/filter/Six.java
@@ -12,6 +12,23 @@ public class Six {
 				new Employee("Aniket", 25, "Finance", 34000,"Mumbai"),new Employee("Rushikesh", 27, "HR", 40000,"Hydarabad"),
 				new Employee("Shantanu", 20, "Finance", 25000,"Mumabi"));
 		
+		System.out.println("Filtering employees whose salary is greater than 50000\n");
+		
 		employees.stream().filter(e -> e.getSalary() > 50000).forEach(System.out::println);
+		
+		System.out.println("\n========================================================================================\n");
+		System.out.println("Filtering employees whose age is greater than 30\n");
+		
+		employees.stream().filter(e -> e.getAge() > 30).forEach(System.out::println);
+		
+		System.out.println("\n========================================================================================\n");
+		System.out.println("Filtering employees whose name starts with character D\n");
+		
+		employees.stream().filter(e -> e.getName().startsWith("D")).forEach(System.out::println);
+		
+		System.out.println("\n========================================================================================\n");
+		System.out.println("Filtering employees who live is pune city \n");
+		
+		employees.stream().filter(e -> e.getCity().equals("Pune")).forEach(System.out::println);
 	}
 }
